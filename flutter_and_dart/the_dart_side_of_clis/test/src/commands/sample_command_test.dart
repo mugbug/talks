45ce0c52749bb35454e183f2a1d5cf340803e2e8
Pedro Zaroni <pedro.zaroni@quintoandar.com.br>
2023-01-17 18:06:33 -0300

    talk: run very_good create

---
+++ flutter_and_dart/the_dart_side_of_clis/test/src/commands/sample_command_test.dart
@@ -0,0 +1,72 @@
+import 'package:mason_logger/mason_logger.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:pub_updater/pub_updater.dart';
+import 'package:test/test.dart';
+import 'package:the_dart_side_of_clis/src/command_runner.dart';
+import 'package:the_dart_side_of_clis/src/version.dart';
+
+class MockLogger extends Mock implements Logger {}
+
+class MockPubUpdater extends Mock implements PubUpdater {}
+
+void main() {
+  group('sample', () {
+    late PubUpdater pubUpdater;
+    late Logger logger;
+    late TheDartSideOfClisCommandRunner commandRunner;
+
+    setUp(() {
+      pubUpdater = MockPubUpdater();
+
+      when(
+        () => pubUpdater.getLatestVersion(any()),
+      ).thenAnswer((_) async => packageVersion);
+
+      logger = MockLogger();
+      commandRunner = TheDartSideOfClisCommandRunner(
+        logger: logger,
+        pubUpdater: pubUpdater,
+      );
+    });
+
+    test('tells a joke', () async {
+      final exitCode = await commandRunner.run(['sample']);
+
+      expect(exitCode, ExitCode.success.code);
+
+      verify(
+        () => logger.info('Which unicorn has a cold? The Achoo-nicorn!'),
+      ).called(1);
+    });
+    test('tells a joke in cyan', () async {
+      final exitCode = await commandRunner.run(['sample', '-c']);
+
+      expect(exitCode, ExitCode.success.code);
+
+      verify(
+        () => logger.info(
+          lightCyan.wrap('Which unicorn has a cold? The Achoo-nicorn!'),
+        ),
+      ).called(1);
+    });
+
+    test('wrong usage', () async {
+      final exitCode = await commandRunner.run(['sample', '-p']);
+
+      expect(exitCode, ExitCode.usage.code);
+
+      verify(() => logger.err('Could not find an option or flag "-p".'))
+          .called(1);
+      verify(
+        () => logger.info(
+          '''
+Usage: $executableName sample [arguments]
+-h, --help    Print this usage information.
+-c, --cyan    Prints the same joke, but in cyan
+
+Run "$executableName help" to see global options.''',
+        ),
+      ).called(1);
+    });
+  });
+}
